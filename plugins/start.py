f12c44036395aad61e02c55342e6f5905d37dc25
Siiiid01 <153982728+Siiiid01@users.noreply.github.com>
2025-02-07 16:58:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -211,7 +211,7 @@ async def start_command(client: Client, message: Message):
             ]
         )
         await message.reply_photo(
-            photo=START_PIC,
+            photo=random.choice(PICS),
             caption=START_MSG.format(
                 first=message.from_user.first_name,
                 last=message.from_user.last_name,
