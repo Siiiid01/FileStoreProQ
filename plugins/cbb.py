080acb4c5ceb781184054ae4625a5cf419fde88a
QuirkySiiiiiid <jain96488@gmail.com>
2025-02-17 15:06:16 +0530

    custom msg try

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -50,7 +50,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             media=InputMediaPhoto(
                 random.choice(PICS),  # New random image every time
                 caption=MORE_TXT.format(first=query.from_user.first_name),
-                 parse_mode="html"  # Ensure proper formatting
+                parse_mode="html"  # Ensure proper formatting
             ),
             reply_markup=InlineKeyboardMarkup([
                 [InlineKeyboardButton("• ʜᴇʟᴘ •", callback_data='help'),
