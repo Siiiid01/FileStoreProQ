20ca05709d8b285807826e3e48fdf70fb02e51c8
Siiiid01 <153982728+Siiiid01@users.noreply.github.com>
2025-03-04 17:40:08 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -10,7 +10,7 @@ from helper_func import encode, check_user_ban
     filters.private 
     & filters.user(ADMINS) 
     & ~filters.command([
-        "start", "id", "users", "broadcast", "batch", "genlink", "stats", "telegraph", "alive", "ping", "stickerid", "errors", "ban", "unban", "feedback", "restart"
+        "start", "id", "users", "broadcast", "batch", "genlink", "stats", "telegraph", "alive", "ping", "stickerid", "errors", "ban", "unban", "feedback", "restart", "react", "react_now"
     ])
 )
 async def channel_post(client: Client, message: Message):
